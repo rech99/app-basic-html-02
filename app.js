9186add4593d0888c74bc1310fc25585201175f0
rech99 <rech_99@hotmail.com>
2019-09-06 13:09:01 -0700

    cambios

---
+++ app.js
@@ -0,0 +1,11 @@
+/*const doge = document.getElementById('doge');
+doge.addEventListener('click', ()=>{
+    console.log('Guau, guau.');
+});
+const drumButtons = document.querySelectorAll('button');
+drumButtons.forEach(
+        button =>{
+            button.style.left = `${button.dataset.posx}px`; 
+            button.style.top = `${button.dataset.posy}px`;
+        } 
+);*/
